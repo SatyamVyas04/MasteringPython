0e513c09212b65bfcfefbf5f05f83cc902a6c2aa
SatyamVyas <vyas.satyam.04@gmail.com>
2022-09-27 22:43:29 +0530

    new file created

--- Openpyxl/app2.py
+++ Openpyxl/app2.py
@@ -4,7 +4,5 @@ import openpyxl as xl
 wb = xl.load_workbook('transactions2.xlsx')
 # loading a singular sheet
 sheet = wb["Sheet1"]
-# assigning a variable the cell value
-cell = sheet["a1"]  # or "cell = sheet.cell(1, 1)"
 
 # adding a graph to the data
