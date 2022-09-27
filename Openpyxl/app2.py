01e1cbf7fc7c1bdb5070e1c1e6ff9b24076a528d
SatyamVyas <vyas.satyam.04@gmail.com>
2022-09-27 23:27:05 +0530

    barchart added

--- Openpyxl/app2.py
+++ Openpyxl/app2.py
@@ -1,8 +1,20 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 # loading Workbook
-wb = xl.load_workbook('transactions2.xlsx')
+wb = xl.load_workbook('Openpyxl/transactions2.xlsx')
 # loading a singular sheet
 sheet = wb["Sheet1"]
 
 # adding a graph to the data
+values = Reference(sheet, 
+                   min_row=2, 
+                   max_row=sheet.max_row,
+                   min_col=4, 
+                   max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, "e2")
+
+wb.save("transactions3.xlsx")
