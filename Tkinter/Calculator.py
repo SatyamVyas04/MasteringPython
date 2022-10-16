6bb2481cd7b6cb6619ef24372d0b1ea459119131
SatyamVyas <vyas.satyam.04@gmail.com>
2022-10-16 19:08:41 +0530

    Made a simple Calculator

--- Tkinter/Calculator.py
+++ Tkinter/Calculator.py
@@ -1,4 +1,5 @@
 from cgitb import text
+from multiprocessing.sharedctypes import Value
 from tkinter import *
 
 root = Tk()
@@ -7,27 +8,46 @@ root.title("Simple Calculator") # renaming the Tk window
 e = Entry(root, width=50, borderwidth=5) # Creating the Bar where numbers show
 e.grid(row=0, column=0, columnspan=4, rowspan=2, padx=10, pady=10) # displaying it
 
+calc = ""
 def click(text): # writing what the Buttons do
-    # e.delete(0, END)
-    e.insert(1, text)
+    global calc
+    if text in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
+        current = e.get()
+        e.delete(0, END)
+        e.insert(0, current + text)
+        calc+=text
+    
+    if text=="=":
+        e.delete(0, END)
+        calc = f"{eval(calc)}"
+        e.insert(0, eval(calc))
+        
+    elif text in ["+", "-", "/", "*"]:
+        e.delete(0, END)
+        calc+=text
+        
+def clear():
+    global calc
+    e.delete(0, END)
+    calc = ""
 
 # Creating the buttons
-b1 = Button(root, text="1", padx=40, pady=20, command=lambda: click("1"), fg="yellow")
-b2 = Button(root, text="2", padx=40, pady=20, command=lambda: click("2"), fg="yellow")
-b3 = Button(root, text="3", padx=40, pady=20, command=lambda: click("3"), fg="yellow")
-b4 = Button(root, text="4", padx=40, pady=20, command=lambda: click("4"), fg="yellow")
-b5 = Button(root, text="5", padx=40, pady=20, command=lambda: click("5"), fg="yellow")
-b6 = Button(root, text="6", padx=40, pady=20, command=lambda: click("6"), fg="yellow")
-b7 = Button(root, text="7", padx=40, pady=20, command=lambda: click("7"), fg="yellow")
-b8 = Button(root, text="8", padx=40, pady=20, command=lambda: click("8"), fg="yellow")
-b9 = Button(root, text="9", padx=40, pady=20, command=lambda: click("9"), fg="yellow")
-b0 = Button(root, text="0", padx=40, pady=20, command=lambda: click("0"), fg="yellow") 
-bcls = Button(root, text="cls", padx=35, pady=20, command=lambda: click("C"), fg="Blue") 
-bequal = Button(root, text="=", padx=40, pady=20, command=lambda: click("="), fg="Black")
-badd = Button(root, text="+", padx=40, pady=20, command=lambda: click("+"), fg="Black")
-bsub = Button(root, text="-", padx=40, pady=20, command=lambda: click("-"), fg="Black")
-bmult = Button(root, text="*", padx=40, pady=20, command=lambda: click("*"), fg="Black")
-bdiv = Button(root, text="/", padx=40, pady=20, command=lambda: click("/"), fg="Black")
+b1 = Button(root, text="1", padx=40, pady=20, command=lambda: click("1"), fg="#00FFFF")
+b2 = Button(root, text="2", padx=40, pady=20, command=lambda: click("2"), fg="#00FFFF")
+b3 = Button(root, text="3", padx=40, pady=20, command=lambda: click("3"), fg="#00FFFF")
+b4 = Button(root, text="4", padx=40, pady=20, command=lambda: click("4"), fg="#00FFFF")
+b5 = Button(root, text="5", padx=40, pady=20, command=lambda: click("5"), fg="#00FFFF")
+b6 = Button(root, text="6", padx=40, pady=20, command=lambda: click("6"), fg="#00FFFF")
+b7 = Button(root, text="7", padx=40, pady=20, command=lambda: click("7"), fg="#00FFFF")
+b8 = Button(root, text="8", padx=40, pady=20, command=lambda: click("8"), fg="#00FFFF")
+b9 = Button(root, text="9", padx=40, pady=20, command=lambda: click("9"), fg="#00FFFF")
+b0 = Button(root, text="0", padx=40, pady=20, command=lambda: click("0"), fg="#00FFFF") 
+bcls = Button(root, text="CLS", padx=32, pady=20, command=clear, fg="Black") 
+bequal = Button(root, text="=", padx=40, pady=20, command=lambda: click("="), fg="Yellow")
+badd = Button(root, text="+", padx=40, pady=20, command=lambda: click("+"), fg="Yellow")
+bsub = Button(root, text="-", padx=40, pady=20, command=lambda: click("-"), fg="Yellow")
+bmult = Button(root, text="*", padx=40, pady=20, command=lambda: click("*"), fg="Yellow")
+bdiv = Button(root, text="/", padx=40, pady=20, command=lambda: click("/"), fg="Yellow")
 
 
 # Placing the buttons in the Tk window
