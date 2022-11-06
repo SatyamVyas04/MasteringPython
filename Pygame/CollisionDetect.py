64619350893535724a9841907d10d1bb2085c925
SatyamVyas <vyas.satyam.04@gmail.com>
2022-11-06 20:03:34 +0530

    Made resizable window

--- Pygame/CollisionDetect.py
+++ Pygame/CollisionDetect.py
@@ -2,22 +2,11 @@ import sys, pygame, random
 pygame.init()
 
 # Window Setup
-wnW = int(input("Enter Menu Width: "))
-if wnW >=1500:
-    wnW = 1500
-    
-wnH = int(input("Enter Window Height: "))
-if wnH >= 750:
-    wnH = 750
-
-surface = pygame.display.set_mode((wnW, wnH))
+wnW = 1000
+wnH = 700
+surface = pygame.display.set_mode((wnW, wnH), pygame.RESIZABLE)
 pygame.display.set_caption("CollisionDetection")
 clock = pygame.time.Clock()
-
-# Menu Setup
-menuW, menuH = wnW, int(wnH/5)
-menu = pygame.Surface((menuW, menuH))
-menu.fill("Gold")
 
 # Menu Buttons Setup
 # Plan: 4 buttons for [Circle Mouse interaction,
@@ -28,8 +17,14 @@ menu.fill("Gold")
 running = True
 while running:
     surface.fill("White")
-    # Menu
-    surface.blit(menu, (0, wnH-menuH))
+    # Resize Check:
+    wnW, wnH = surface.get_size()
+    # Menu Setup
+    menuW, menuH = wnW, int(wnH/5)
+    menu = pygame.Surface((menuW, menuH))
+    menu.fill("Gold")
+    menu_TL_Y = wnH - menuH
+    surface.blit(menu, (0, menu_TL_Y))
     
     
     
