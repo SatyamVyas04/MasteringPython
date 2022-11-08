c01137ff76e72624440deb06a59cd02442b7b5fe
SatyamVyas <vyas.satyam.04@gmail.com>
2022-11-08 23:26:50 +0530

    Removed Resizable feature

--- Pygame/CollisionDetect.py
+++ Pygame/CollisionDetect.py
@@ -2,48 +2,37 @@ import sys, pygame, random
 pygame.init()
 
 # Window Setup
-wnW = 1000
+wnW = 700
 wnH = 700
-surface = pygame.display.set_mode((wnW, wnH), pygame.RESIZABLE)
+surface = pygame.display.set_mode((wnW, wnH))
 pygame.display.set_caption("CollisionDetection")
 clock = pygame.time.Clock()
 
 # Menu Buttons Setup
-# Plan: 4 buttons for [Circle Mouse interaction,
-#                      Square Mouse interaction, 
-#                      Square Square interaction,
-#                      Circle Circle interaction] 
+menuW, menuH = wnW, int(wnH/5)
+padding = 0.1*menuH
+sidepadding = 3.6*padding
+menu = pygame.Surface((menuW, menuH))
+menu.fill("Gold")
+menu_TL_Y = wnH - menuH
+surface.blit(menu, (0, menu_TL_Y))
+
+# Buttons Setup
+bH = bW = 0.8*menuH
+pygame.draw.rect(surface, "blue", pygame.Rect(sidepadding, menu_TL_Y+padding, bW, bH))
+pygame.draw.rect(surface, "blue", pygame.Rect(2*sidepadding+bW, menu_TL_Y+padding, bW, bH))
+pygame.draw.rect(surface, "blue", pygame.Rect(3*sidepadding+2*bW, menu_TL_Y+padding, bW, bH))
+pygame.draw.rect(surface, "blue", pygame.Rect(4*sidepadding+3*bW, menu_TL_Y+padding, bW, bH))
 
 running = True
 while running:
-    surface.fill("White")
-    # Resize Check:
-    wnW, wnH = surface.get_size()
-    surface = pygame.display.set_mode((wnW, wnH), pygame.RESIZABLE)
-    # Menu Setup
-    menuW, menuH = wnW, int(wnH/5)
-    menu = pygame.Surface((menuW, menuH))
-    menu.fill("Gold")
-    menu_TL_Y = wnH - menuH
-    surface.blit(menu, (0, menu_TL_Y))
-    # Buttons Setup
-    bH = bW = 0.8*menuH
-    pygame.draw.rect(surface, "blue", pygame.Rect(0.1*menuH, menu_TL_Y+0.1*menuH, bW, bH))
-    
+
     
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.VIDEORESIZE:
-            width, height = event.size
-            if width < 1000:
-                width = 1000
-            if height < 625:
-                height = 625
-            wnW, wnH = width, height
-
 
     pygame.display.update()
     clock.tick(60)
